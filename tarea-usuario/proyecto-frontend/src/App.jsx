74bc077d8e855ae168c6468a1e47471f4d53d0ba
Cristian Elgueta <cristian.elgueta@usach.cl>
2024-04-19 16:12:39 -0400

    Se crean nuevas rutas para Registro, Login y Home

--- tarea-usuario/proyecto-frontend/src/App.jsx
+++ tarea-usuario/proyecto-frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import { Formik } from 'formik'
-import './App.css'
+//import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './components/pages/home/Home'
+import Register from './components/pages/register/Register'
+import Login from './components/pages/login/Login'
 
 function App() {
 
   return (
-    console.log("ola")
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
-export default App
+export default App;
